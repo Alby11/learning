5060b3e000e963bdb80d24fc21719fb1ea46b319
S4rg0n <albertotallone@gmail.com>
2016-06-19 13:52:32 +0200

    added generators

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ var myWorker1 = new Worker('worker1.js');
 var myWorker2 = new Worker('worker2.js');
 
 let sendToWorker1 = function () {
-    setTimeout(() => myWorker1.postMessage('Message to myWorker1'), 0);
+
+    setInterval(()=>setTimeout(() => myWorker1.postMessage('Message to myWorker1'), 0), 1000);
 }
 
 let sendToWorker2 = function () {
@@ -19,6 +20,27 @@ myWorker2.onmessage = (message) => {
     console.log(message.data);
 }
 
-setInterval(()=>sendToWorker1(), 1000);
+function* aGenerator1() {
+    for (let i = 0; i <= 10; i++) {
+        let aYield = yield;
+        console.log(aYield + i);
+    }
+    console.log('FINISHED 1');
+}
+
+function* aGenerator2() {
+    for (let i = 0; i <= 10; i++) {
+        let aYield = yield;
+        console.log(i + aYield);
+    }
+    console.log('FINISHED 2');
+}
+
+var yiedTest1 = aGenerator1();
+var yiedTest2 = aGenerator2();
+
+document.getElementById('log').addEventListener('click', sendToWorker1);
 document.getElementById('loop').addEventListener('click', sendToWorker2);
 document.getElementById('resp').addEventListener('click', ()=>console.log(`I'm Alive!!!!!`));
+document.getElementById('yield1').addEventListener('click', ()=>yiedTest1.next('Loop '));
+document.getElementById('yield2').addEventListener('click', ()=>yiedTest2.next(100));
