6289a9a672420708e955ac6f2f1a43608e83a286
Alberto Tallone <albertotallone@gmail.com>
2016-06-21 12:13:54 +0200

    yield*

--- generators.js
+++ generators.js
@@ -1,18 +1,20 @@
 function* foo() {
-  console.log('In foo');
-  var c = yield* bar(1.618);
-  console.log(c);
+    console.log('In foo');
+    var c = yield* bar(1.618);
+    console.log(c);
 }
 
 function* bar(param) {
-  console.log('In bar');
-  yield param;
-  return 42;
+    console.log('In bar');
+    yield 'test';
+    yield param;
+    return 42;
 }
 
 var f = foo();
-f.next();
-f.next();
+console.log(f.next());
+console.log(f.next());
+console.log(f.next());
 /**
   // In foo
   // In bar
